9c25e3a9f6e4fab3aa83cca2dfc388cd75a36757
Olya <varentsova.olga@gmail.com>
2020-06-17 11:33:19 +0300

    change table

--- DNA/temperatureMelting.py
+++ DNA/temperatureMelting.py
@@ -9,8 +9,7 @@ class MeltingTemp:
         self.Ca = Ca
         self.Cb = Cb
 
-    table_of_nucleotids = [
-        {
+    table_of_nucleotids =  {
             'AA': (-7.9, -22.2),
             'TT': (-7.9, -22.2),
             'AT': (-7.2, -20.4),
@@ -23,33 +22,20 @@ class MeltingTemp:
             'GC': (-9.8, -24.4),
             'GG': (-8.0, -19.9),
             'CC': (-8.0, -19.9)
-        },
-        {
-            'TT': (-7.9, -22.2),
-            'AA': (-7.9, -22.2),
-            'TA': (-7.2, -20.4),
-            'AT': (-7.2, -21.3),
-            'GT': (-8.5, -22.7),
-            'CA': (-8.4, -22.4),
-            'GA': (-7.8, -21.0),
-            'CT': (-8.2, -22.2),
-            'GC': (-10.6, -27.2),
-            'CG': (-9.8, -24.4),
-            'CC': (-8.0, -19.9),
-            'GG': (-8.0, -19.9)
         }
 
-    ]
+
+
     def sum_dH_dS(self):  # count dH and dS
         for strand in self.dna:
             n = self.dna.index(strand)
             k = len(strand)
             i = 0
             while i < k - 1:
-                for key in self.table_of_nucleotids[n]:
+                for key in self.table_of_nucleotids:
                     if self.dna[n][i] + self.dna[n][i + 1] == key:
-                        self.dH += self.table_of_nucleotids[n][key][0]
-                        self.dS += self.table_of_nucleotids[n][key][1]
+                        self.dH += self.table_of_nucleotids[key][0]
+                        self.dS += self.table_of_nucleotids[key][1]
                 i = i + 1
 
     def tm(self):
